428d2403820933864ba2ff4831c18b57513c07a4
xiezhuosong <xiezhuosong@gmail.com>
2025-02-17 22:28:23 +0800

    modify index,vercel.json,vite.config.js

--- app/web/vite.config.js
+++ app/web/vite.config.js
@@ -3,6 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/', // 确保是根路径（默认配置）
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    outDir: 'dist' // 必须与 vercel.json 的 distDir 一致
+  },
   plugins: [vue()],
   envDir: '../', // 指定环境文件查找目录
   envPrefix: 'VITE_', // 只暴露VITE_开头的环境变量
